4a89ea8856e5c933d638a7cc8102f2223c50ce26
yanlipnican <jan.lipnican@productboard.com>
2025-03-08 22:57:14 +0100

    2025

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      detection_methods = { "pattern" },
+      patterns = { ".git" },
+    },
+  },
+}
